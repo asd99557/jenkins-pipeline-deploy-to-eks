1eb8f4b4cce43946299048bb4031f088c262bc95
asd99557 <asd99557@gmail.com>
2024-07-22 18:57:34 +0530

    jenkins-pipeline-deploy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,7 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
-variable "region_number" {
-  # Arbitrary mapping of region name to number to use in
-  # a VPC's CIDR prefix.
-  default = {
-    us-east-1      = 1
-    us-east-2      = 1
-  }
-  }
+variable "availability_zone" {
+ type = string
+ description = "To set AWS availability region"
+}
