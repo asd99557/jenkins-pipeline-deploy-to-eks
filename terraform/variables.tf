17f969af0649b28e5185c1a3b8dbb23d7e5e2ea2
asd99557 <asd99557@gmail.com>
2024-07-22 17:57:39 +0530

    jenkins-pipeline-deploy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,11 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
+variable "region_number" {
+  # Arbitrary mapping of region name to number to use in
+  # a VPC's CIDR prefix.
+  default = {
+    us-east-1      = 1a
+    us-east-2      = 1b
+  }
+  }
