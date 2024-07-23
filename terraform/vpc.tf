d5cdfaa8acc84d5f08bbf89cf77028dff3af85b7
asd99557 <asd99557@gmail.com>
2024-07-23 17:04:56 +0530

    jenkins-pipeline-deploy

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,7 +7,7 @@ module "myapp-vpc" {
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
   #azs             = data.aws_availability_zones.azs.names
-  azs               = ["us-east-1a","us-east-1b"]           
+  azs          = ["us-east-1a", "us-east-1b"]           
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
